baf868a9bf72d6c98a69ea9856b648bc5f91b9e4
dawidkaszuba <dawidk89@gmail.com>
2021-11-28 21:36:09 +0100

    added routes

--- src/main/java/pl/dawidkaszuba/gatewayserver/GatewayserverApplication.java
+++ src/main/java/pl/dawidkaszuba/gatewayserver/GatewayserverApplication.java
@@ -2,12 +2,28 @@ package pl.dawidkaszuba.gatewayserver;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class GatewayserverApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+
+	@Bean
+	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
+		return builder.routes()
+				.route(p-> p
+						.path("/users/**")
+						.uri("lb://USER-SERVICE"))
+				.route(p -> p
+						.path("/expenses/**")
+						.uri("lb://EXPENSE-SERVICE")).build();
+	}
 }

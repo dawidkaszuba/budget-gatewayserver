d9f6f036d335517a71cb9e0c3090f70ad9991597
dawidkaszuba <dawidk89@gmail.com>
2023-02-12 22:44:22 +0100

    draft

--- src/main/java/pl/dawidkaszuba/gatewayserver/GatewayserverApplication.java
+++ src/main/java/pl/dawidkaszuba/gatewayserver/GatewayserverApplication.java
@@ -1,7 +1,9 @@
 package pl.dawidkaszuba.gatewayserver;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.gateway.filter.factory.TokenRelayGatewayFilterFactory;
 import org.springframework.cloud.gateway.route.RouteLocator;
 import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
@@ -13,15 +15,19 @@ public class GatewayserverApplication {
 		SpringApplication.run(GatewayserverApplication.class, args);
 	}
 
+	@Autowired
+	TokenRelayGatewayFilterFactory filterFactory;
 
 	@Bean
 	public RouteLocator myRoutes(RouteLocatorBuilder builder) {
 		return builder.routes()
 				.route(p-> p
 						.path("/budget/**")
+						.filters(f -> f.filters(filterFactory.apply()))
 						.uri("lb://BUDGETSERVICE"))
 				.route(p -> p
 						.path("/report/**")
+						.filters(f -> f.filters(filterFactory.apply()))
 						.uri("lb://REPORTSERVICE")).build();
 	}
 }
